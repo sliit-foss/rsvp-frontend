cd94987f582168e2e65b6adf1a1a0e5b4f1af488
Layan Jayasinghe <layanjayasinghe2@gmail.com>
2021-06-24 08:06:29 +0530

    tailwind and react-icons added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,7 @@
 import '../styles/globals.css'
+import '@fontsource/inter/400.css'
+import '@fontsource/inter/200.css'
+import '@fontsource/inter/600.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,28 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      black: '#121212',
+      textBlack: '#202020',
+      white: '#ffffff',
+      blue: '#005BDC',
+      bgWhite: '#F4F3F5',
+      gradientBlue: '#21B3D0',
+      gradientPurple: '#7B50A2',
+      gray: {
+        default: '#6C7075',
+        light: '#DFDFDF',
+      }
+    },
+    fontFamily: {
+      inter: ['Inter', 'sans-serif'],
+    },
     extend: {},
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
