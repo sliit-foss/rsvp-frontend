6f6705908ab0b641044da82b490ed9f16e8ef2f6
Madhusha Prasad <50085447+MadhushaPrasad@users.noreply.github.com>
2021-06-25 17:00:49 +0530

    Update index.tsx
    fixed import components order

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,9 @@ import Navbar from '../components/Navbar'
 // modules
 import HeroSection from '../modules/HomePage/hero'
 import MakeGoodThings from '../modules/HomePage/makeGoodThings'
-import Footer from '../components/Footer'
-import OurPartners from '../modules/HomePage/ourPartners'
 import WhatWeDo from '../modules/HomePage/WhatWeDo'
+import OurPartners from '../modules/HomePage/ourPartners'
+import Footer from '../components/Footer'
 
 const Homepage = () => {
   return (
