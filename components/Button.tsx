9d69097e934475743bb0689cd8787fc7793e9a84
MadhushaPrasad <madushaprasad21@gmail.com>
2021-06-26 14:08:07 +0530

    change button interface

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,16 +1,16 @@
-interface props {
-  value: string
+interface ButtonProps {
+    value: string
 }
 
-const Button = ({ value }: props) => {
-  return (
-    <button
-      className='py-2 px-8 md:bg-gradientBlue bg-blue rounded-lg shadow-md hover:bg-gradientPurple duration-150 transition ease-in font-medium'
-      type='button'
-    >
-      {value}
-    </button>
-  )
+const Button = ({value}: ButtonProps) => {
+    return (
+        <button
+            className='py-2 px-8 md:bg-gradientBlue bg-blue rounded-lg shadow-md hover:bg-gradientPurple duration-150 transition ease-in font-medium'
+            type='button'
+        >
+            {value}
+        </button>
+    )
 };
 
 export default Button
