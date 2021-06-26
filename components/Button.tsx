2a96781d437941278ca54a938d5685d54a3009ad
Sithum Bopitiya <sithumsandeepap@gmail.com>
2021-06-26 18:03:13 +0530

    feature/husky lint (#28)
    * Fix css build issue
    
    * Add husky and prettier
    
    * Add prettier config
    
    * Add as dev deps
    
    * Update eslintrc
    
    * Add plugin
    
    * Add tslint
    
    * Fix eslint errors

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,16 +1,16 @@
-interface ButtonProps {
-    value: string
+interface props {
+  value: string
 }
 
-const Button = ({value}: ButtonProps) => {
-    return (
-        <button
-            className='py-2 px-8 md:bg-gradientBlue bg-blue rounded-lg shadow-md hover:bg-gradientPurple duration-150 transition ease-in font-medium'
-            type='button'
-        >
-            {value}
-        </button>
-    )
+const Button = ({ value }: props): JSX.Element => {
+  return (
+    <button
+      className="py-2 px-8 md:bg-gradientBlue bg-blue rounded-lg shadow-md hover:bg-gradientPurple duration-150 transition ease-in font-medium"
+      type="button"
+    >
+      {value}
+    </button>
+  );
 };
 
-export default Button
+export default Button;
