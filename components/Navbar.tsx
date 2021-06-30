a1544674f0ecbda3f48e0c34d52018cbe2c86ca5
Layan Jayasinghe <layanjayasinghe2@gmail.com>
2021-06-30 06:57:11 +0530

    Header animations added.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,23 +1,27 @@
-import { useState } from "react";
+import { useState } from 'react'
+import { AnimatePresence, motion } from 'framer-motion'
 
-import Image from "next/image";
-import Link from "next/link";
+import Image from 'next/image'
+import Link from 'next/link'
 
-import Container from "./Layout/Container";
+import Container from './Layout/Container'
 
-import foss_dark from "../public/logos/foss_dark.svg";
-import foss_light from "../public/logos/foss_light.svg";
+import foss_dark from '../public/logos/foss_dark.svg'
+import foss_light from '../public/logos/foss_light.svg'
+
+// animations
+import { mobileNavVariants, navElementsVariants } from '../animations'
 
 // icons
-import { HiMenuAlt2 } from "react-icons/hi";
-import { RiCloseFill } from "react-icons/ri";
+import { HiMenuAlt2 } from 'react-icons/hi'
+import { RiCloseFill } from 'react-icons/ri'
 
 const Navbar = (): JSX.Element => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false)
 
   const toggleNav = () => {
-    setIsOpen((prev) => !prev);
-  };
+    setIsOpen((prev) => !prev)
+  }
 
   return (
     <header className="relative">
@@ -68,77 +72,95 @@ const Navbar = (): JSX.Element => {
                 {!isOpen ? (
                   <HiMenuAlt2 className="h-6 w-6 hover:text-gray-default transition ease-in" />
                 ) : (
-                  ""
+                  ''
                 )}
               </li>
             </ul>
           </nav>
         </div>
-        {isOpen ? (
-          <div
-            className={`fixed top-0 left-0 z-10 ${
-              isOpen ? `min-h-screen` : `h-0`
-            } w-full transition-all ease-in bg-blue flex flex-col items-center justify-center`}
-          >
-            <div
-              className="absolute top-4 right-4 cursor-pointer"
-              onClick={toggleNav}
+        <AnimatePresence exitBeforeEnter>
+          {isOpen ? (
+            <motion.div
+              initial="initial"
+              animate="animate"
+              exit="exit"
+              variants={mobileNavVariants}
+              className={`fixed top-0 left-0 bottom-0 z-10 min-h-screen ${
+                isOpen ? `w-full` : `w-0`
+              } bg-blue flex flex-col items-center justify-center`}
             >
-              {isOpen ? (
-                <RiCloseFill className="h-6 w-6 hover:text-gray-light text-white transition ease-in" />
-              ) : (
-                ""
-              )}
-            </div>
-            <div className="h-32 w-48 flex items-center mb-5">
-              <Image
-                src={foss_light}
-                alt="foss logo"
-                layout="intrinsic"
-                quality={90}
-              />
-            </div>
-            <ul className="flex flex-col items-center justify-center space-y-6">
-              <li
-                className="font-medium text-2xl hover:text-gray-light text-white transition ease-in"
-                onClick={() => setIsOpen(false)}
+              <div
+                className="absolute top-4 right-4 cursor-pointer"
+                onClick={toggleNav}
               >
-                <Link href="/">
-                  <a>Home</a>
-                </Link>
-              </li>
-              <li
-                className="font-medium text-2xl hover:text-gray-light text-white transition ease-in"
-                onClick={() => setIsOpen(false)}
+                {isOpen ? (
+                  <RiCloseFill className="h-6 w-6 hover:text-gray-light text-white transition ease-in" />
+                ) : (
+                  ''
+                )}
+              </div>
+              <motion.div
+                initial="initial"
+                animate="animate"
+                exit="exit"
+                variants={navElementsVariants}
+                className="h-32 w-48 flex items-center mb-5"
               >
-                <Link href="/event">
-                  <a>Events</a>
-                </Link>
-              </li>
-              <li
-                className="font-medium text-2xl hover:text-gray-light text-white transition ease-in"
-                onClick={() => setIsOpen(false)}
+                <Image
+                  src={foss_light}
+                  alt="foss logo"
+                  layout="intrinsic"
+                  quality={90}
+                />
+              </motion.div>
+              <motion.ul
+                initial="initial"
+                animate="animate"
+                exit="exit"
+                variants={navElementsVariants}
+                className="flex flex-col items-center justify-center space-y-6"
               >
-                <Link href="/clubs">
-                  <a>Clubs</a>
-                </Link>
-              </li>
-              <li
-                className="font-medium text-2xl hover:text-gray-light text-white transition ease-in"
-                onClick={() => setIsOpen(false)}
-              >
-                <Link href="/contact">
-                  <a>Contact</a>
-                </Link>
-              </li>
-            </ul>
-          </div>
-        ) : (
-          ""
-        )}
+                <li
+                  className="font-medium text-2xl hover:text-gray-light text-white transition ease-in"
+                  onClick={() => setIsOpen(false)}
+                >
+                  <Link href="/">
+                    <a>Home</a>
+                  </Link>
+                </li>
+                <li
+                  className="font-medium text-2xl hover:text-gray-light text-white transition ease-in"
+                  onClick={() => setIsOpen(false)}
+                >
+                  <Link href="/event">
+                    <a>Events</a>
+                  </Link>
+                </li>
+                <li
+                  className="font-medium text-2xl hover:text-gray-light text-white transition ease-in"
+                  onClick={() => setIsOpen(false)}
+                >
+                  <Link href="/clubs">
+                    <a>Clubs</a>
+                  </Link>
+                </li>
+                <li
+                  className="font-medium text-2xl hover:text-gray-light text-white transition ease-in"
+                  onClick={() => setIsOpen(false)}
+                >
+                  <Link href="/contact">
+                    <a>Contact</a>
+                  </Link>
+                </li>
+              </motion.ul>
+            </motion.div>
+          ) : (
+            ''
+          )}
+        </AnimatePresence>
       </Container>
     </header>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
