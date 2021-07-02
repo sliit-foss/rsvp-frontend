0da73e3a3891a11d6cfac817810a5e3356c9da00
Akalanka Perera <akalankaperera128@gmail.com>
2021-07-02 22:23:20 +0530

    resolved conflicts

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -51,7 +51,7 @@ const Navbar = (): JSX.Element => {
                 </Link>
               </li>
               <li className="hidden lg:block font-medium text-lg hover:text-gray-default transition ease-in">
-                <Link href="/event">
+                <Link href="/events">
                   <a>Events</a>
                 </Link>
               </li>
@@ -132,7 +132,7 @@ const Navbar = (): JSX.Element => {
                   className="font-medium text-2xl hover:text-gray-light text-white transition ease-in"
                   onClick={() => setIsOpen(false)}
                 >
-                  <Link href="/event">
+                  <Link href="/events">
                     <a>Events</a>
                   </Link>
                 </li>
