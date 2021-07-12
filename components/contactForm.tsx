77c6accd9bfc69006e244401c02cea0fed2a6128
MadhushaPrasad <madushaprasad21@gmail.com>
2021-07-12 19:07:47 +0530

    remove semicolon

---
+++ components/contactForm.tsx
@@ -0,0 +1,15 @@
+import Container from './Layout/Container'
+
+const ContactForm = (): JSX.Element => {
+  return (
+    <div>
+      <Container>
+        <div className="bg-redAccent w-full">
+          <h1>Madhusha</h1>
+        </div>
+      </Container>
+    </div>
+  )
+}
+
+export default ContactForm
