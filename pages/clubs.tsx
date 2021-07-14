bb42a5d52c8297720a0c7141dd6869372cb206fc
Madhusha Prasad <50085447+MadhushaPrasad@users.noreply.github.com>
2021-07-14 15:34:30 +0530

    Feature/clubs (#56)
    * footer finished
    
    * change scroll bar colors
    
    * change subscribe text padding and set footer to home page
    
    * Footer #2
    
    * change .gitignore
    
    * Update index.tsx
    
    fixed import components order
    
    * marge
    
    * some comment
    
    * create club component
    
    * create club component props interface
    
    * change button interface
    
    * change logo props variable type
    
    * add infocards box shadow
    
    * Contact Us Info Cards Section Done #11
    
    * remove contact us semicolon
    
    * remove contact us semicolon
    
    * remove semicolon
    
    * remove semicolon
    
    * remove semicolon
    
    * add missed mobile responsive elements
    
    * remove semicolon
    
    * remove semicolon
    
    * Change file name first letter to capital

--- pages/clubs.tsx
+++ pages/clubs.tsx
@@ -1,16 +1,15 @@
-// components
 import Layout from '../components/Layout'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import Container from '../components/Layout/Container'
 import Club from '../components/Club'
 
-// images
 import foss from '../public/clubs/foss.png'
 import mozilla from '../public/clubs/mozilla.png'
 import ms from '../public/clubs/ms.png'
 import sliit from '../public/clubs/sliit.png'
 import wif from '../public/clubs/wif.png'
+import { HiArrowRight } from 'react-icons/hi'
 
 const ClubsPage = (): JSX.Element => {
   return (
@@ -21,6 +20,17 @@ const ClubsPage = (): JSX.Element => {
         className="bg-no-repeat bg-right-top bg-cover py-10"
       >
         <Container>
+          <div className="w-full lg:hidden mb-16 mt-20">
+            <h1
+              className="flex flex-row items-center text-3xl
+            font-semibold"
+            >
+              Clubs{''}
+              <span className="inline-block">
+                <HiArrowRight className="ml-3" />
+              </span>
+            </h1>
+          </div>
           <div className="grid grid-cols-2 grid-rows-1 md:gap-8 sm:gap-6 gap-5 max-w-4xl mx-auto">
             <Club logo={foss} title="SLIIT FOSS Community" />
             <Club logo={mozilla} title="Mozilla Campus Club" />
--- pages/contactUs.tsx
+++ pages/contactUs.tsx
@@ -1,8 +1,10 @@
 import InfoCards from '../components/InfoCards'
+import ContactForm from '../components/contactForm'
 
 const ContactUs = (): JSX.Element => {
   return (
     <div className="">
+      <ContactForm />
       <InfoCards />
     </div>
   )
