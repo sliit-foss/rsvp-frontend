dee9635a0f50505bc6ae546aba2f9e653fdaf6ea
Akalanka Perera <akalankaperera128@gmail.com>
2021-07-15 17:29:54 +0530

    hotfix/footer (#64)
    Fixed spelling mistakes in footer and added links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import fossLogo from '../public/logos/foss_light.svg'
 import {
   FaFacebook,
@@ -18,20 +19,39 @@ const Footer = (): JSX.Element => {
         <div className="relative -top-8">
           <div>
             <p className="pl-10 pr-10 text-white mb-3">
-            A group of volunteers who believe in the 
-            usage of Free/Open Source Software (FOSS). 
-            The primary objective of the community is 
-            to promote, develop and diversify the usage 
-            of Free and Open Source Software at SLIIT.
+              A group of volunteers who believe in the usage of Free/Open Source
+              Software (FOSS). The primary objective of the community is to
+              promote, develop and diversify the usage of Free and Open Source
+              Software at SLIIT.
             </p>
           </div>
           <div className="pl-10 pr-10 text-white mb-3 ">
-            <h4 className="text-xl font-bold mb-3">Foloow Us</h4>
+            <h4 className="text-xl font-bold mb-3">Follow Us</h4>
             <div className="sm:mb-3 md:mb-3 flex flex-row">
-              <FaFacebook className="colorIcon mb-3 mr-3" size="22" />
-              <FaInstagram className="colorIcon mb-3  mr-3" size="22" />
-              <FaLinkedin className="colorIcon mb-3  mr-3" size="22" />
-              <FaTwitter className="colorIcon mb-3  mr-3" size="22" />
+              <a href="">
+                <FaFacebook
+                  className="colorIcon mb-3 mr-3 fill-current-color hover:text-white"
+                  size="22"
+                />
+              </a>
+              <a href="">
+                <FaInstagram
+                  className="colorIcon mb-3  mr-3 fill-current-color hover:text-white"
+                  size="22"
+                />
+              </a>
+              <a href="">
+                <FaLinkedin
+                  className="colorIcon mb-3  mr-3 fill-current-color hover:text-white"
+                  size="22"
+                />
+              </a>
+              <a href="">
+                <FaTwitter
+                  className="colorIcon mb-3  mr-3 fill-current-color hover:text-white"
+                  size="22"
+                />
+              </a>
             </div>
           </div>
         </div>
@@ -50,20 +70,60 @@ const Footer = (): JSX.Element => {
           <div className="flex flex-row w-full text-white pl-10 pr-10">
             <div className="sm:mt-5 sm:mb-5 md:mt-5 md:mb-5 w-1/2">
               <ul>
-                <li className="pt-3 pb-3">Home</li>
-                <li className="pt-3 pb-3">Clubs</li>
-                <li className="pt-3 pb-3">About Us</li>
-                <li className="pt-3 pb-3">Contact Us</li>
-                <li className="pt-3 pb-3">Events</li>
+                <li className="pt-3 pb-3">
+                  <Link href="/">
+                    <a>Home</a>
+                  </Link>
+                </li>
+                <li className="pt-3 pb-3">
+                  <Link href="/clubs">
+                    <a>Clubs</a>
+                  </Link>
+                </li>
+                <li className="pt-3 pb-3">
+                  <Link href="/">
+                    <a>About Us</a>
+                  </Link>
+                </li>
+                <li className="pt-3 pb-3">
+                  <Link href="/contact">
+                    <a>Contact Us</a>
+                  </Link>
+                </li>
+                <li className="pt-3 pb-3">
+                  <Link href="/events">
+                    <a>Events</a>
+                  </Link>
+                </li>
               </ul>
             </div>
             <div className="sm:mt-5 sm:mb-5 md:mt-5 md:mb-5 w-1/2">
               <ul>
-                <li className="pt-3 pb-3">Home</li>
-                <li className="pt-3 pb-3">Clubs</li>
-                <li className="pt-3 pb-3">About Us</li>
-                <li className="pt-3 pb-3">Contact Us</li>
-                <li className="pt-3 pb-3">Events</li>
+                <li className="pt-3 pb-3">
+                  <Link href="/">
+                    <a>Home</a>
+                  </Link>
+                </li>
+                <li className="pt-3 pb-3">
+                  <Link href="/clubs">
+                    <a>Clubs</a>
+                  </Link>
+                </li>
+                <li className="pt-3 pb-3">
+                  <Link href="/">
+                    <a>About Us</a>
+                  </Link>
+                </li>
+                <li className="pt-3 pb-3">
+                  <Link href="/contact">
+                    <a>Contact Us</a>
+                  </Link>
+                </li>
+                <li className="pt-3 pb-3">
+                  <Link href="/events">
+                    <a>Events</a>
+                  </Link>
+                </li>
               </ul>
             </div>
           </div>
@@ -74,7 +134,7 @@ const Footer = (): JSX.Element => {
         <div className="w-full h-1/4 md:mb-4 lg:mb-3">
           <div className="lg:pt-14 pb-5">
             <h4 className="relative left-10 w-1/2 sm:w-1/2 md:w-1/2 text-xl font-bold mb-1 text-white">
-              Subcribe
+              Subscribe
             </h4>
             <span className="absolute left-10 bg-white w-16 h-1 rounded-xl" />
           </div>
@@ -93,7 +153,7 @@ const Footer = (): JSX.Element => {
                   type="email"
                   placeholder="Email Address"
                   required
-                  className="p-2 mt-5 w-3/4 rounded-lg block text-black shadow-md"
+                  className="p-2 pl-3 mt-5 w-3/4 rounded-lg block text-black shadow-md"
                 />
                 <button className="w-11 h-11  bg-blue mt-5 shadow-md hover:bg-gradientPurple duration-150 transition ease-in font-medium rounded-lg block relative -left-7">
                   <FaRegEnvelope
