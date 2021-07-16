6f830344d0e058053c0840535a66193fad6bde63
MadhushaPrasad <madushaprasad21@gmail.com>
2021-07-17 01:50:27 +0530

    Contact Page Done

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -2,17 +2,20 @@ import ContactForm from '../components/ContactForm'
 import Footer from '../components/Footer'
 import InfoCards from '../components/InfoCards'
 import Navbar from '../components/Navbar'
+import Container from '../components/Layout/Container'
 
 const Contact = (): JSX.Element => {
   return (
-    <div className="">
+    <div className=''>
       <Navbar />
       <section
         style={{ backgroundImage: 'url(/patterns/clubs.svg)' }}
-        className="bg-no-repeat bg-right-top bg-cover py-10"
+        className='bg-no-repeat bg-right-top bg-cover py-10'
       >
         <ContactForm />
-        <InfoCards />
+        <Container>
+          <InfoCards />
+        </Container>
       </section>
       <Footer />
     </div>
