99518b8f9cac29c213f35d195c9f13484b52cd6a
Madhusha Prasad <50085447+MadhushaPrasad@users.noreply.github.com>
2021-07-17 08:51:45 +0530

    Feature/contact form (#66)
    * footer finished
    
    * change scroll bar colors
    
    * change subscribe text padding and set footer to home page
    
    * Footer #2
    
    * change .gitignore
    
    * Update index.tsx
    
    fixed import components order
    
    * marge
    
    * some comment
    
    * Fix css build issue
    
    * create club component
    
    * create club component props interface
    
    * change button interface
    
    * change logo props variable type
    
    * Add husky and prettier
    
    * Add prettier config
    
    * Add as dev deps
    
    * Update eslintrc
    
    * Add plugin
    
    * Add tslint
    
    * Fix src errors
    
    * add infocards box shadow
    
    * Contact Us Info Cards Section Done #11
    
    * remove contact us semicolon
    
    * remove contact us semicolon
    
    * remove semicolon
    
    * remove semicolon
    
    * remove semicolon
    
    * add missed mobile responsive elements
    
    * remove semicolon
    
    * remove semicolon
    
    * Add sticky style to header
    
    * Update contactUs.tsx
    
    * Update contactUs.tsx
    
    * fixed all issues
    
    * remove comments
    
    * install react-share
    
    * import icon and buttons
    
    * Create media icon to share
    
    * Create a contact folder to store vecotor
    
    * change ContactForm.tsx
    
    * ContactForm done
    
    * Contact Page Done
    
    * Resolve conflicts
    
    Co-authored-by: Sithum Bopitiya <sithumsandeepap@gmail.com>

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,12 +1,21 @@
+import ContactForm from '../components/ContactForm'
 import Footer from '../components/Footer'
 import InfoCards from '../components/InfoCards'
 import Navbar from '../components/Navbar'
+import Container from '../components/Layout/Container'
 
 const Contact = (): JSX.Element => {
   return (
-    <div className="">
+    <div className=''>
       <Navbar />
-      <InfoCards />
+      <section
+        style={{ backgroundImage: 'url(/patterns/clubs.svg)' }}
+        className='bg-no-repeat bg-right-top bg-cover py-10'>
+        <ContactForm />
+        <Container>
+          <InfoCards />
+        </Container>
+      </section>
       <Footer />
     </div>
   )
