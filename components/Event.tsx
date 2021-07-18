d7ca3394c018da91e791764546bde5d7ca924b89
Akalanka Perera <akalankaperera128@gmail.com>
2021-07-18 15:56:06 +0530

    feature/single-event-page (#69)
    added single event page

--- components/Event.tsx
+++ components/Event.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react'
+import Link from 'next/link'
 import Aos from 'aos'
 import 'aos/dist/aos.css'
 import Image from 'next/image'
@@ -33,7 +34,7 @@ const Event = ({
 
   return (
     <div data-aos="fade-up" className="w-full lg:w-2/6  md:px-4 lg:px-6 py-5 ">
-      <div className="bg-white hover:shadow-xl h-full rounded-xl">
+      <div className="bg-white hover:shadow-xl h-full rounded-xl transition-all ease-out duration-500">
         <Image
           className="border-white border-8 hover:opacity-25 rounded-xl rounded-b-none transition-all ease-out duration-500"
           src={logo}
@@ -45,12 +46,12 @@ const Event = ({
         <div className="px-4 py-4 md:px-10">
           <h1 className="font-bold text-lg">{title}</h1>
           <p className="py-4">{description}</p>
-          <a
-            href=""
-            className="w-full mb-1 md:w-full text-base font-bold text-gray-dark hover:text-lightBlueAccent"
-          >
-            Read More
-          </a>
+          <Link href="/singleEvent">
+            <a className="w-full mb-1 md:w-full text-base font-bold text-gray-dark hover:text-lightBlueAccent">
+              Read More
+            </a>
+          </Link>
+
           <div className="flex flex-wrap pt-8">
             <div className="text-sm font-medium  flex flex-wrap">
               <div className="bg-lightBlueAccent py-0.5 px-4 mb-4 rounded-3xl shadow-md text-white text-sm">
