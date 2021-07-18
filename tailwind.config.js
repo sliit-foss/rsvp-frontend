1e088d57aa15e975fea84b888a8a69f01e9c4ca1
Akalanka Perera <akalankaperera128@gmail.com>
2021-07-18 21:12:03 +0530

    hotfix/tailwind-config
    fixed issue in purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   purge: [
-    './pages/**/*.{js,ts,jsx,tsx}v',
+    './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
     './modules/**/*.{js,ts,jsx,tsx}',
   ],
