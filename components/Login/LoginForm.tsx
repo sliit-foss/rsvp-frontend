b0f250ad0412cd7d097f4894991e6086e80475a3
Layan Jayasinghe <layanjayasinghe2@gmail.com>
2021-07-19 21:13:13 +0530

    fix: react-forms implementation

--- components/Login/LoginForm.tsx
+++ components/Login/LoginForm.tsx
@@ -27,7 +27,9 @@ const LoginForm = ({
   const [showLoading, setShowLoading] = useState(false)
   const [rememberMeValue, setRememberMeValue] = useState(false)
 
-  const { errors } = useForm()
+  const {
+    formState: { errors },
+  } = useForm()
 
   if (Object.keys(errors).length > 0) {
     console.log('Err : ', errors)
@@ -72,13 +74,13 @@ const LoginForm = ({
     <>
       <LoadingOverlay show={showLoading} />
       <div className="flex flex-col order-1 lg:order-none items-center lg:w-6/12 lg:justify-center lg:transform lg:scale-90">
-        <h2 className="font-inter font-extrabold text-blue font-inter text-3xl lg:text-2xl mb-2">
+        <h2 className="font-inter font-extrabold text-blue text-3xl lg:text-2xl mb-2">
           {login ? 'LOGIN' : 'Sign Up'}
         </h2>
         <p className="font-inter text-textBlackSecondary font-medium mb-6 text-sm">
           {login ? 'Login' : 'Sign up'} to access the best things!
         </p>
-        <button className="shadow-ds2 flex flex-row w-full lg:w-4/5 h-10 rounded-lg flex items-center justify-center leading-4 font-semibold font-inter text-sm mb-6">
+        <button className="shadow-ds2 flex flex-row w-full lg:w-4/5 h-10 rounded-lg items-center justify-center leading-4 font-semibold font-inter text-sm mb-6">
           <NextImage
             classnames="w-4 h-4 mr-4 line"
             src={googleLogo}
