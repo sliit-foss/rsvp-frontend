e3178dced095a73452468abc8c663036a14d6303
Akalanka Perera <akalankaperera128@gmail.com>
2021-07-21 18:34:18 +0530

    feature/particles.js

---
+++ components/Common/ParticleBG.tsx
@@ -0,0 +1,88 @@
+import Particles from 'react-particles-js'
+const ParticleBG = (): JSX.Element => {
+  return (
+    <Particles
+      className="absolute top-0 w-full h-full "
+      params={{
+        background: {
+          color: {
+            value: 'rgba(255, 255, 255,0,0)',
+          },
+        },
+        fpsLimit: 60,
+        interactivity: {
+          detectsOn: 'canvas',
+          events: {
+            onClick: {
+              enable: true,
+              mode: 'push',
+            },
+            onHover: {
+              enable: true,
+              mode: 'repulse',
+            },
+            resize: true,
+          },
+          modes: {
+            bubble: {
+              distance: 400,
+              duration: 2,
+              opacity: 0.8,
+              size: 40,
+            },
+            push: {
+              quantity: 4,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: '#005BDC',
+          },
+          links: {
+            color: '#005BDC',
+            distance: 35,
+            enable: true,
+            opacity: 0.9,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: 'none',
+            enable: true,
+            outMode: 'bounce',
+            random: false,
+            speed: 2,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              value_area: 1500,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: 'circle',
+          },
+          size: {
+            random: true,
+            value: 5,
+          },
+        },
+        detectRetina: true,
+      }}
+    />
+  )
+}
+
+export default ParticleBG
--- modules/HomePage/MakeGoodThings.tsx
+++ modules/HomePage/MakeGoodThings.tsx
@@ -1,59 +1,65 @@
 import { useEffect } from 'react'
 import Aos from 'aos'
 import 'aos/dist/aos.css'
+
 import Container from '../../components/Layout/Container'
 import NextImage from '../../components/NextImage'
 
 import makeGoodThings from '../../public/make-good-things.svg'
+import ParticleBG from '../../components/Common/ParticleBG'
 
 const MakeGoodThings = (): JSX.Element => {
   useEffect(() => {
     Aos.init({ offset: 0, duration: 1000 })
   }, [])
-  return (
-    <Container>
-      <section className="grid grid-cols-1 grid-rows-1 md:grid-cols-2 gap-6 md:py-24 py-16">
-        <div data-aos="fade-right">
-          <NextImage
-            classnames="md:col-span-1 flex items-center justify-center max-w-lg lg:w-auto sm:w-80 w-64 mx-auto my-8"
-            src={makeGoodThings}
-            alt="make good things"
-            layout="intrinsic"
-            quality={90}
-          />
-        </div>
 
-        <div
-          className="md:col-span-1 flex flex-col justify-center"
-          data-aos="fade-left"
-        >
-          <h2 className="text-4xl sm:text-5xl font-bold text-blue md:text-left text-center leading-snug mb-6">
-            Make Good Things Together
-          </h2>
-          <p className="md:text-left text-center text-gray-default max-w-lg md:mx-0 mx-auto mb-5">
-            Here, at SLIIT, our intention is to improve people&apos;s life, and
-            as we see education is the greatest opportunity that can be given to
-            a person. We want to create a better society, where we can all
-            develop our maximum potential through what we are passionate about.
-            We talk to opinion leaders, philosophers, industrial experts and
-            teachers who help us to be better people. We want to give society
-            tools, experiences and knowledge so that all people can develop and
-            have the best opportunities.
-          </p>
-          <div className="inline-flex justify-center md:justify-start space-x-2">
-            <div className="text-gray-default text-xs bg-gray-light py-1 px-3 rounded-2xl shadow-md">
-              #SLIITFOSS
-            </div>
-            <div className="text-gray-default text-xs bg-gray-light py-1 px-3 rounded-2xl shadow-md">
-              #MSCLUBOFSLIIT
-            </div>
-            <div className="text-gray-default text-xs bg-gray-light py-1 px-3 rounded-2xl shadow-md">
-              #SLIITFCSC
+  return (
+    <div className="relative">
+      <ParticleBG />
+      <Container>
+        <section className="grid grid-cols-1 grid-rows-1 md:grid-cols-2 gap-6 md:py-24 py-16 z-10">
+          <div data-aos="fade-right">
+            <NextImage
+              classnames="md:col-span-1 flex items-center justify-center max-w-lg lg:w-auto sm:w-80 w-64 mx-auto my-8"
+              src={makeGoodThings}
+              alt="make good things"
+              layout="intrinsic"
+              quality={90}
+            />
+          </div>
+
+          <div
+            className="md:col-span-1 flex flex-col justify-center"
+            data-aos="fade-left"
+          >
+            <h2 className="text-4xl sm:text-5xl font-bold text-blue md:text-left text-center leading-snug mb-6">
+              Make Good Things Together
+            </h2>
+            <p className="md:text-left text-center text-gray-default max-w-lg md:mx-0 mx-auto mb-5">
+              Here, at SLIIT, our intention is to improve people&apos;s life,
+              and as we see education is the greatest opportunity that can be
+              given to a person. We want to create a better society, where we
+              can all develop our maximum potential through what we are
+              passionate about. We talk to opinion leaders, philosophers,
+              industrial experts and teachers who help us to be better people.
+              We want to give society tools, experiences and knowledge so that
+              all people can develop and have the best opportunities.
+            </p>
+            <div className="inline-flex justify-center md:justify-start space-x-2">
+              <div className="text-gray-default text-xs bg-gray-light py-1 px-3 rounded-2xl shadow-md cursor-default filter hover:brightness-105 transform hover:scale-105 transition ease-in duration-200">
+                #SLIITFOSS
+              </div>
+              <div className="text-gray-default text-xs bg-gray-light py-1 px-3 rounded-2xl shadow-md cursor-default filter hover:brightness-105 transform hover:scale-105 transition ease-in duration-200">
+                #MSCLUBOFSLIIT
+              </div>
+              <div className="text-gray-default text-xs bg-gray-light py-1 px-3 rounded-2xl shadow-md cursor-default filter hover:brightness-105 transform hover:scale-105 transition ease-in duration-200">
+                #SLIITFCSC
+              </div>
             </div>
           </div>
-        </div>
-      </section>
-    </Container>
+        </section>
+      </Container>
+    </div>
   )
 }
 
