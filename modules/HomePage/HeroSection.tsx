9f42115a3621fe64ad7e54fa966fb821ba3f50dc
Akalanka Perera <akalankaperera128@gmail.com>
2021-07-30 09:31:59 +0530

    feature/hero-carousel-UI

--- modules/HomePage/HeroSection.tsx
+++ modules/HomePage/HeroSection.tsx
@@ -1,47 +1,28 @@
-import Container from '../../components/Layout/Container'
-import Button from '../../components/Button'
-import { HiArrowRight } from 'react-icons/hi'
+import 'react-responsive-carousel/lib/styles/carousel.min.css' // requires a loader
+import { Carousel } from 'react-responsive-carousel'
+import EventBanner from '../../components/EventBanner'
 
 const HeroSection = (): JSX.Element => {
+  const latestEvents = [0, 1, 2, 3, 4]
   return (
     <section className="bg-gradient-to-r from-gradientBlue to-gradientPurple shadow-lg">
-      <Container>
-        <section className="text-white grid grid-cols-1 grid-rows-1 md:grid-cols-2 gap-6 md:py-44 py-20">
-          <div className="md:col-span-1 flex flex-col justify-center">
-            <p className="inline-flex items-center font-medium text-2xl sm:text-3xl mb-8 animate-pulse">
-              Upcoming{' '}
-              <span>
-                <HiArrowRight className="ml-3" />
-              </span>
-            </p>
-            <h1 className="font-bold text-4xl sm:text-5xl lg:text-7xl mb-5 leading-snug">
-              JavaScript Fundamentals
-            </h1>
-            <div className="inline-flex items-center space-x-2">
-              <p className="font-medium">Programming</p>
-              <div className="text-gray-default text-xs sm:text-base bg-gray-light py-1 px-3 rounded-2xl shadow-md cursor-default filter hover:brightness-105 transition ease-in duration-200">
-                #JavaScript
-              </div>
-              <div className="text-gray-default text-xs sm:text-base bg-gray-light py-1 px-3 rounded-2xl shadow-md cursor-default filter hover:brightness-105 transition ease-in duration-200">
-                #SLIITFOSS
-              </div>
-            </div>
-          </div>
-          <div className="md:col-span-1 flex flex-col justify-center items-start ">
-            <p className="text-2xl sm:text-3xl font-medium mb-6">Join us on</p>
-            <h2 className="text-3xl sm:text-4xl font-bold mb-5">
-              November 8th
-            </h2>
-            <p className="mb-6 max-w-lg">
-              {`Callbacks and closures are used frequently in JavaScript. Callbacks are
-                functions that are passed into another function as an argument. Closures
-                are functions that are nested in other functions, and it's often used to 
-                avoid scope clash with other parts of a JavaScript program.`}
-            </p>
-            <Button value={'RSVP'} />
-          </div>
-        </section>
-      </Container>
+      <Carousel
+        className=""
+        showThumbs={false}
+        showArrows={false}
+        showStatus={false}
+        showIndicators={false}
+        autoPlay={true}
+        interval={5000}
+        transitionTime={1000}
+        infiniteLoop={true}
+        emulateTouch={true}
+        swipeable={true}
+      >
+        {latestEvents.map((index) => (
+          <EventBanner key={index} />
+        ))}
+      </Carousel>
     </section>
   )
 }
