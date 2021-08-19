8f926e72a5fa0092651e3d76474eead3b2ed257d
Layan Jayasinghe <layanjayasinghe2@gmail.com>
2021-08-19 22:41:30 +0530

    add: user module

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,12 +1,21 @@
 interface ButtonProps {
   value: string
+  onClick?: () => void
+  width?: string
+  type?: 'button' | 'submit' | 'reset' | undefined
 }
 
-const Button = ({ value }: ButtonProps): JSX.Element => {
+const Button = ({
+  value,
+  onClick,
+  width,
+  type = 'button',
+}: ButtonProps): JSX.Element => {
   return (
     <button
-      className="py-2 px-8 text-white bg-gradientBlue rounded-lg shadow-md hover:bg-gradientPurple duration-150 transition ease-in font-medium"
-      type="button"
+      onClick={onClick}
+      className={`py-2 px-8 ${width} text-white bg-gradientBlue rounded-lg shadow-md hover:bg-gradientPurple duration-150 transition ease-in font-medium`}
+      type={type}
     >
       {value}
     </button>
