1bd83865ae4da71ca437e1e8178edf7c996b75bf
Akalanka Perera <akalanka@parallax.lk>
2022-01-31 06:39:18 +0530

    Updated homepage clubs (#137)

--- modules/HomePage/OurPartners.tsx
+++ modules/HomePage/OurPartners.tsx
@@ -1,6 +1,6 @@
 import Container from '../../components/Layout/Container'
 import NextImage from '../../components/Common/NextImage'
-import { logoData } from '../../lib/LogoData'
+import { clubs } from '../../lib/clubData'
 import ParticleBG from '../../components/Common/ParticleBG'
 
 const OurPartners = (): JSX.Element => {
@@ -25,20 +25,23 @@ const OurPartners = (): JSX.Element => {
             partnership. If you’re interested in being showcased throughout ,
             contact SLIIT FCSC to discuss sponsorship opportunities.
           </p>
-          <div className="inline-flex justify-center items-center space-x-2 sm:space-x-3 lg:space-x-6">
-            {logoData.map(({ image, alt }, index) => (
+          <div className="inline-flex justify-center items-center space-x-3 lg:space-x-6 flex-wrap mb-8">
+            {clubs.map(({ logo, title, url }, index) => (
               <div
                 key={index}
                 data-aos={index % 2 == 0 ? 'fade-up' : 'fade-down'}
                 data-aos-offset={120}
               >
-                <NextImage
-                  classnames="flex items-center justify-center w-9 sm:w-16 md:w-20 lg:w-28 my-8"
-                  src={image}
-                  alt={alt}
-                  layout="intrinsic"
-                  quality={90}
-                />
+                <a href={url} rel="noreferrer" target="_blank">
+                  <NextImage
+                    classnames="flex items-center justify-center w-16 md:w-20 lg:w-28 mt-8 shadow-sm rounded-lg transform hover:scale-105 transition-all ease-out duration-300"
+                    nextImgClassnames="rounded-lg"
+                    src={logo}
+                    alt={title}
+                    layout="intrinsic"
+                    quality={90}
+                  />
+                </a>
               </div>
             ))}
           </div>
--- pages/clubs.tsx
+++ pages/clubs.tsx
@@ -3,58 +3,10 @@ import Navbar from '../components/Layout/Navbar'
 import Footer from '../components/Layout/Footer'
 import Container from '../components/Layout/Container'
 import Club from '../components/Club'
-import foss from '../public/clubs/foss.png'
-import fcsc from '../public/clubs/fcsc.png'
-import mozilla from '../public/clubs/mozilla.png'
-import ms from '../public/clubs/ms.png'
-import media from '../public/clubs/media.png'
-import cs2 from '../public/clubs/cs2.png'
-import sesc from '../public/clubs/sesc.png'
-import wif from '../public/clubs/wif.png'
+import { clubs } from '../lib/clubData'
 import { HiArrowRight } from 'react-icons/hi'
 
-const clubs = [
-  {
-    title: 'SLIIT FOSS Community',
-    logo: foss,
-    url: 'https://sliitfoss.org',
-  },
-  {
-    title: 'FCSC',
-    logo: fcsc,
-    url: 'https://www.facebook.com/sliit.fcsc/',
-  },
-  {
-    title: 'MS Club of SLIIT',
-    logo: ms,
-    url: 'https://www.facebook.com/msclubsliit/',
-  },
-  {
-    title: 'Media Unit',
-    logo: media,
-    url: 'https://www.facebook.com/sliit.fcmu/',
-  },
-  {
-    title: 'Mozilla Campus Club',
-    logo: mozilla,
-    url: 'https://www.facebook.com/sliitmcc/',
-  },
-  {
-    title: 'Cyber Security Community',
-    logo: cs2,
-    url: 'https://www.facebook.com/sliit.cscs/',
-  },
-  {
-    title: 'SESC',
-    logo: sesc,
-    url: 'https://www.facebook.com/sliitsecommunity/',
-  },
-  {
-    title: 'SLIIT Women in FOSS Community',
-    logo: wif,
-    url: 'https://wif-web.web.app'
-  },
-]
+
 
 const ClubsPage = (): JSX.Element => {
   return (
