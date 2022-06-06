626b7be4a59a4358d78355773593e16ecd57457c
MadhushaPrasad <madushaprasad21@gmail.com>
2022-06-06 17:30:34 +0530

    fixed cl issue

--- components/Login/LoginInfo.tsx
+++ components/Login/LoginInfo.tsx
@@ -9,8 +9,6 @@ interface LoginInfoProps {
 const LoginInfo = (props: LoginInfoProps): JSX.Element => {
   const outerContainerClass =
     'flex flex-col items-center justify-center lg:justify-center lg:w-6/12 lg:items-center'
-  const paragraphClass =
-    'text-center text-textBlackTertiary mb-6 lg:text-left lg:w-5/6 lg:text-sm lg:p-6'
   const imageClass = 'mb-6 lg:mb-0 w-3/4 lg:w-5/6'
 
   if (props.login) {
