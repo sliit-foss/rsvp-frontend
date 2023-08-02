4e336e84b6e6a9a50efe778123efcbdd0cea2f80
Akalanka Perera <akalankaperera128@gmail.com>
2023-08-02 20:34:07 +0530

    Chore: updated api base url (#143)

--- pages/api/apiInstance.ts
+++ pages/api/apiInstance.ts
@@ -6,7 +6,7 @@ const apiInstance = axios.create({
 
 apiInstance.interceptors.request.use(
   async function (config) {
-    config.baseURL = 'https://rsvp-backend.herokuapp.com'
+    config.baseURL = 'https://api-rsvp.cyclic.cloud'
     config.headers = {
       Authorization: localStorage.getItem('Token'),
     }
